f6dc43c182a48372b07f5f5a43cb20e4c4f271c1
GusPe27 <gustavoperezc27@gmail.com>
2022-06-15 03:13:18 -0500

    AdaptacionReferenciaAlDashboard

--- src/js/tooglebtn.js
+++ src/js/tooglebtn.js
@@ -2,4 +2,5 @@ const btnToggle = document.querySelector('.toggle-btn');
 
 btnToggle.addEventListener('click',function(){
     document.getElementById('sidebar').classList.toggle('active');
+    document.getElementById('cuerpo').classList.toggle('active');
 });
